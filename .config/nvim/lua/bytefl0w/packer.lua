2fb870d5de183c20f53ef98af03c585871c77061
Erik Mortimer <hello@erikmortimer.dev>
2022-06-24 07:49:31 -0400

    feat: Implemented autocomplete and lsp for rust and golang

--- .config/nvim/lua/bytefl0w/packer.lua
+++ .config/nvim/lua/bytefl0w/packer.lua
@@ -6,6 +6,8 @@ return require("packer").startup(function()
     use("hrsh7th/cmp-nvim-lsp")
     use("hrsh7th/cmp-buffer")
     use("hrsh7th/cmp-path")
+    use("L3MON4D3/LuaSnip")
+    use("saadparwaiz1/cmp_luasnip")
 
     -- TODO: add and learn to use "nvim-telescope/telescope.nvim" and 
     -- "ThePrimeagen/harpoon"
